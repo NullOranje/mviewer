ddb7d997fa953a8d36d6f7c488c7d1af917d0fd0
Nick McKinney <nick@ndmckinney.net>
2026-02-03 16:58:27 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+@app.route('/static')
+def static_sample():
+    return render_template("static.html")
+
+if __name__ == '__main__':
+    app.run()
